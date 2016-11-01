2ca974b31509feeb3375a36495354b5c31eceec3
= <=>
2016-11-01 09:36:22 -0700

    Added Fibonacci Solutions

---
+++ FibonacciSolutions.py
@@ -0,0 +1,36 @@
+def FibonacciUsingRecursion(n):
+    
+    # Base case
+    if n == 0 or n == 1:
+        return 1
+    else:
+        # Recurse
+        return FibonacciUsingRecursion(n-1) + FibonacciUsingRecursion(n-2)
+
+def FibonacciUsingIteration(n):
+    
+    previousNumber = 0
+    sum = 1
+    
+    for i in range(1,n): 
+        sum += previousNumber
+        previousNumber = sum - previousNumber
+    return sum
+
+# 0, 1, 1, 2, 3, 5
+# a,b = b,a+b
+# 0,1 = 1,1
+# b,c = c,b+c
+# 1,1 = 1,2
+# c,d = d,c+d
+# 1,2 = 2,3
+
+def fib_iter(n):
+    
+    a,b = 0,1
+    
+    for i in range(n):
+        
+        a,b = b,a+b
+    
+    return a
--- PermuteString.py
+++ PermuteString.py
@@ -1,7 +1,6 @@
 import random, math
 
 def permute(s):
-    print("CALLED")
     out = []
     
     # Base Case
@@ -12,21 +11,11 @@ def permute(s):
     else:
         
         # for every letter in string
-        print("TOPTOPTOPTOPTOPTOPTOPTOPTOP")
         for i, letter in enumerate(s):
-            
-            print("--------------------: " + letter + ", " + str(i))
-            #print(out)
-            #print("reset complete")
-            #print(str(i) + ";" + letter)
-            print(s[:i] + s[i+1:])
+   
             # For every permutation resulting from Step 2 and 3
             for permutation in permute( s[:i] + s[i+1:] ) :
-                print("____________________")
-                print(letter + permutation)
+ 
                 out += [letter + permutation]
-                #print("**: " + letter + permutation)
-                #print(str(i) + ";{L}:" + letter +";{p}:" + permutation)
-                #print(out)
-                
+            
     return out
